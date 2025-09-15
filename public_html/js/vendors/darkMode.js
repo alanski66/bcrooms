376417af860db658c0102649fea755c78d17525a
joomkit <alan@craftcode.co.uk>
2025-09-15 01:45:20 +0100

    darkmode fix

--- public_html/js/vendors/darkMode.js
+++ public_html/js/vendors/darkMode.js
@@ -12,14 +12,14 @@
             return storedTheme;
         }
 
-        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "light";
     };
 
     const setTheme = (theme) => {
         if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
             document.documentElement.setAttribute("data-bs-theme", "dark");
         } else {
-            document.documentElement.setAttribute("data-bs-theme", theme);
+            document.documentElement.setAttribute("data-bs-theme", "light");
         }
     };
 
